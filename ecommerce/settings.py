512b498eee6b6aa923615dcdcaced50636dd4e47
bhoomi gaheja <bhoomigaheja.ps12@gmail.com>
2024-05-19 17:23:25 +0530

    first commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -126,12 +126,12 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'ecom_web_app/static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, "static"),
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     MEDIA_ROOT = os.environ.get('MEDIA_ROOT', os.path.join(BASE_DIR, 'media'))
-    MEDIA_URL = '/media/'
+    MEDIA_URL = 'ecom_web_app/media/'
 
 LOGIN_REDIRECT_URL = '/afterlogin'
 
