ad1a47cd1abcb528a3371bb297c662426ecdf543
bhoomi gaheja <bhoomigaheja.ps12@gmail.com>
2024-05-19 16:25:19 +0530

    first commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -132,8 +132,13 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-MEDIA_ROOT=os.path.join(BASE_DIR,'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [STATIC_DIR,]
+# Media files (Uploaded Files)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 LOGIN_REDIRECT_URL='/afterlogin'
